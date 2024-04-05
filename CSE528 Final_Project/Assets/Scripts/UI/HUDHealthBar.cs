15bfe7e32e494017bb9aedad90b3ca06dd1f3704
t0phil04 <t0phil04@louisville.edu>
2024-04-05 14:01:26 -0400

    New UI Screens Added
    I have added the following:
    
             - MainMenu to LevelSelector_ scene transition
             - LevelSelector_ back to MainMenu scene transition
             - UI HUD for LevelSelector_ scene with 3 buttons to change levels
             - LevelSelector_ to Level1_ scene transition
             - UI HUD for Level1_ scene with health bar, credits, round counter, tower selection, and sell tower placeholders

---
+++ CSE528 Final_Project/Assets/Scripts/UI/HUDHealthBar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDHealthBar : MonoBehaviour
+{
+    [Tooltip("Image component displaying current health")]
+    public Image HealthFillImage;
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthBar();
+    }
+
+    // Method to decrease player's health
+    public void DecreaseHealth(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        UpdateHealthBar();
+    }
+
+    // Update the health bar display
+    private void UpdateHealthBar()
+    {
+        HealthFillImage.fillAmount = (float)currentHealth / maxHealth;
+    }
+}
