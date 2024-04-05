82e64396a5c521248c4646c134332048aaea44ef
t0phil04 <t0phil04@louisville.edu>
2024-04-04 21:41:21 -0400

    UI Additons
    Added Features:
    
    1. Start menu screen with 2 working button
    
    2. Levels button will properly take to to another scene (actual level selection UI to be created later)
    
    3. Directions button will bring up a popup window with directions text.
               - Included is a close button that will close the directions window
    
    4. Added a Title referencing the game name
    
    5. Importing a possible tower asset with their related prefabs, materials and projectiles

---
+++ CSE528 Final_Project/LoadSceneButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+namespace UI
+{
+    public class LoadNewScene : MonoBehaviour
+    {
+        public string sceneName = ""; // Renamed to follow Pascal case naming convention
+
+        private void Update()
+        {
+            // Check if left mouse button is clicked and if this GameObject is currently selected by the EventSystem
+            if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject == gameObject)
+            {
+                LoadTargetScene();
+            }
+        }
+
+        public void LoadTargetScene()
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
